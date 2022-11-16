af2e7c0eab5b163f9ee17d0a76bf4fed344f6bb1
Rustlanachain <hamza.sajid@invozone.com>
2022-11-16 22:49:51 +0500

    Importing canvas to the main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ extern crate stdweb;
 mod canvas;
 mod direction;
  
+use canvas::Canvas;
 fn main(){
-    println!("hello world");
+    stdweb::initialize();
+    let canvas = Canvas::new("#canvas", 20, 20);
+
+    canvas.draw(5, 5, "red");
+    canvas.draw(10, 10, "orange");
+    canvas.draw(15, 10, "blue");
+    canvas.draw(10, 15, "purple");
+
+    stdweb::event_loop();
 }
